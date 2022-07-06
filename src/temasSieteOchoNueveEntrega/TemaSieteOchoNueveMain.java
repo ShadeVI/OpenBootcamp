5e030d5d9fa46122e70ab0964db726c8b2a40490
ShadeVI <manuel.t.shade@gmail.com>
2022-07-06 21:15:29 +0100

    completado temas 7,8,9

--- src/temasSieteOchoNueveEntrega/TemaSieteOchoNueveMain.java
+++ src/temasSieteOchoNueveEntrega/TemaSieteOchoNueveMain.java
@@ -1,10 +1,9 @@
 package temasSieteOchoNueveEntrega;
 
-import java.util.Vector;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
-public class Reverse {
+public class TemaSieteOchoNueveMain {
     public static void main(String[] args){
 
         // Dada la función:
@@ -13,7 +12,7 @@ public class Reverse {
         //
         // Escribe el código que devuelva una cadena al revés.
         // Por ejemplo, la cadena "hola mundo", debe retornar "odnum aloh".
-        System.out.println(Reverse.reverse("hola mundo"));
+        System.out.println(TemaSieteOchoNueveMain.reverse("hola mundo"));
 
         // 1. Crea un array unidimensional de Strings y recórrelo, mostrando únicamente sus valores.
         String[] array1 = new String[] {"Primero", "Segundo", "Tercero", "Cuarto", "Quinto"};
@@ -108,6 +107,87 @@ public class Reverse {
         } catch (Exception e){
             System.out.println(e.getMessage() + " ( " + e.getCause() + " )");
         }
+
+        // 8. Utilizando InputStream y PrintStream, crea una función que reciba dos parámetros:
+        // "fileIn" y "fileOut". La tarea de la función será realizar la copia del fichero dado
+        // en el parámetro "fileIn" al fichero dado en "fileOut".
+        /*
+        try{
+            realizarCopia("C:\\Users\\Asus\\IdeaProjects\\OpenBootcamp\\src\\temasSieteOchoNueveEntrega\\test.txt", "C:\\Users\\Asus\\IdeaProjects\\OpenBootcamp\\src\\temasSieteOchoNueveEntrega\\out.txt");
+        } catch (IOException e){
+            System.out.println("captured");
+        }
+        */
+
+        // 9. Sorpréndenos creando un programa de tu elección que utilice
+        //    InputStream, PrintStream, excepciones, un HashMap y un ArrayList, LinkedList o array.
+        try{
+            contadorLetras("C:\\Users\\Asus\\IdeaProjects\\OpenBootcamp\\src\\temasSieteOchoNueveEntrega\\testParte9.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static void contadorLetras(String fichero) throws FileNotFoundException, IOException{
+        HashMap<String, Integer> mapaLetras = new HashMap<>();
+        try{
+            InputStream ficheroEntrada = new FileInputStream(fichero);
+            PrintStream ficheroSalida = new PrintStream("C:\\Users\\Asus\\IdeaProjects\\OpenBootcamp\\src\\temasSieteOchoNueveEntrega\\Resultado.txt");
+            try{
+                int dato;
+                while((dato = ficheroEntrada.read()) > -1){
+                    String datoString = String.valueOf((char) dato).toLowerCase();
+                    if(dato >= 97 && dato <=122){
+                        if(!mapaLetras.containsKey(datoString)) {
+                            mapaLetras.put(datoString, 1);
+                        } else {
+                            mapaLetras.replace(datoString, mapaLetras.get(datoString) + 1);
+                        }
+                    }
+
+                }
+                String[] letras = new String[mapaLetras.size()];
+                int contLetras = 0;
+                for (Map.Entry<String, Integer> par: mapaLetras.entrySet()
+                ) {
+                    ficheroSalida.println(par.getKey() + " = " + par.getValue());
+                    letras[contLetras] = par.getKey();
+                    contLetras++;
+                }
+                System.out.print("Letras: ");
+                for (String letra: letras) {
+                    System.out.print(" " + letra);
+                }
+            } catch (IOException e){
+                throw new IOException("Error IO");
+            }
+            ficheroEntrada.close();
+            ficheroSalida.close();
+        } catch (FileNotFoundException e){
+            throw new FileNotFoundException("Fichero " + fichero + " no encontrado.");
+        }
+    }
+
+    public static void realizarCopia(String fileIn, String fileOut) throws IOException {
+        try{
+            InputStream ficheroIn = new BufferedInputStream(new FileInputStream(fileIn));
+            PrintStream ficheroOut = new PrintStream(fileOut);
+            int dato;
+            while((dato = ficheroIn.read()) != -1){
+                ficheroOut.write(dato);
+            }
+            System.out.println("Copied");
+            ficheroIn.close();
+            ficheroOut.close();
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
     public static String reverse(String texto){
